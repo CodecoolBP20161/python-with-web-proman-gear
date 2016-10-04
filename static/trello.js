b643010c41af8b157734663ead4a6bc0356ef501
matetotesi <mate.totesi@gmail.com>
2016-10-04 11:37:54 +0200

    uniqueId method is ready for use

--- static/trello.js
+++ static/trello.js
@@ -1,3 +1,14 @@
 /**
  * Created by kakacsu on 2016.10.03..
  */
+var Board = new Object();
+Board.title = "";
+Board.id = 0;
+
+var highestId = localStorage.getItem('highestId') || 0;
+function fillBoradDetails(){
+    Board.id = highestId++;
+    highestId = parseInt(highestId++);
+    localStorage.setItem('highestId', highestId);
+    Board.title = prompt("Please give a name to your board");
+}
