92a7f5162261ae6dd85734517b5c2b7b4aaf6769
b-franka <b.franku@gmail.com>
2016-10-04 15:28:37 +0200

    added storage classes, save board and modal hide on-click

--- static/trello.js
+++ static/trello.js
@@ -1,6 +1,6 @@
 function Board(title, id) {
-    this.title = title
-    this.id = id
+    this.title = title;
+    this.id = id;
 }
 
 function generateUniqueId(){
@@ -10,8 +10,57 @@ function generateUniqueId(){
     return highestId
 }
 
-function fillBoardDetails(){
-    var title = prompt("Please give a name to your board");
-    var id = generateUniqueId()
-    var newBoard = new Board(title, id);
+function fillBoardDetails() {
+    var title = $('input').val();
+    var id = generateUniqueId();
+    return new Board(title, id);
 }
+
+var board1 = new Board('egy', 1);
+var board2 = new Board('ketto', 2);
+var board3 = new Board('harom', 3);
+
+var testObject = [board1, board2, board3];
+localStorage.setItem('boards', JSON.stringify(testObject));
+
+function myLocalStorage() {
+
+    this.getBoards= function() {
+        var boardList = localStorage.getItem('boards');
+        boardList = JSON.parse(boardList);
+        return boardList;
+    };
+
+    this.saveBoard = function(board) {
+        var boards = this.getBoards();
+        boards.push(board);
+        localStorage.setItem('boards', JSON.stringify(boards));
+    };
+}
+
+
+
+function myStorage() {
+
+    this.implementation = function () {
+        return new myLocalStorage();
+    };
+
+    this.saveBoard = function (board) {
+        return this.implementation().saveBoard(board);
+    };
+
+    this.getBoards = function () {
+        return this.implementation().getBoards();
+    };
+}
+
+$(document).ready(function() {
+    $('#save_board').click(function() {
+        var board = fillBoardDetails();
+        var storage = new myStorage()
+        storage.saveBoard(board);
+        $('#myModal').modal('hide');
+        $('.row').append('<div class="col-sm-3"><button type="button" class="btn btn-primary btn-block">' + board.title +'</button></div>')
+    });
+});
