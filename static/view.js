732fd58fb85c46c73e4cf82dd178022404d7a6e6
Ivanics Szilárd <ivanics.szilard@gmail.com>
2016-10-18 11:47:30 +0200

    edit button added to boards and cards

--- static/view.js
+++ static/view.js
@@ -13,11 +13,11 @@ var alertMessage = function (label) {
 
 var createBoard = function (id, title) {
         return $('.row').append('<div class="col-xs-12 col-sm-4 col-md-3 col-lg-3 col-xl-2">' +
-            '<button type="button"' + ' class="btn btn-primary btn-block board" id="' + id + '" role="' + title + '">' + title +'<button type="button" id="' + id + '"class="close close-btn" data-dismiss=btn-primary aria-hidden="true">&times;</button></button></div>')
+            '<button type="button"' + ' class="btn btn-primary btn-block board" id="' + id + '" role="' + title + '">' + title +'<button type="button" id="' + id + '"class="close close-btn" data-dismiss=btn-primary aria-hidden="true">&times;</button><p><span class="glyphicon glyphicon-pencil board-pencil" id="' + id + '"></span></p></button></div>')
     };
 
 var createCard = function (id, title, location) {
-        return $('.list-group').append('<li class="list-group-item" id="' + id + '">' + title + '<button type="button" role="' + location + '" id="' + id + '"class="close close-card" data-dismiss=list-group-item aria-hidden="true">&times;</button></li>'
+        return $('.list-group').append('<li class="list-group-item" id="' + id + '">' + title + '<button type="button" role="' + location + '" id="' + id + '"class="close close-card" data-dismiss=list-group-item aria-hidden="true">&times;</button><p><span class="glyphicon glyphicon-pencil card-pencil" id="' + id + '"></span></p></li>'
             );
     };
 
